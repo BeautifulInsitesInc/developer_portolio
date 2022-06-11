b35d7acf1a98bf64c79ada0b2c347e381d202029
Savvy Insights <greg@savvyinsights.io>
2022-06-11 15:03:09 -0400

    css cleanup

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,14 @@
 $(window).scroll(function(e) {
 
     var distanceScrolled = $(this).scrollTop();
+    var blurRatio=distanceScrolled/60;
+    var blurAmount = blurRatio;
+    var lightAmount=100-blurRatio*5;
+    if (blurRatio >15) {blurAmount = 15;}
+    if (lightAmount <15) {lightAmount=15;}
 
-    $('.main').css('backdrop-filter', 'blur('+distanceScrolled/60+'px)');
+    $('.no-blur').css('backdrop-filter', 'blur('+blurAmount+'px)'+'brightness('+lightAmount+'%)');
+    
+    //$('.no-blur').css('backdrop-filter', 'brightness('+lightAmount+'%)');
 
 });
