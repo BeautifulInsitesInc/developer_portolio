a8ef7f3f756d51f5dad5503e2a6246853ec09841
Savvy Insights <greg@savvyinsights.io>
2022-06-11 18:37:06 -0400

    new css applied

--- js/custom.js
+++ js/custom.js
@@ -4,11 +4,13 @@ $(window).scroll(function(e) {
     var blurRatio=distanceScrolled/60;
     var blurAmount = blurRatio;
     var lightAmount=100-blurRatio*5;
+    var textOpacity=(100-blurRatio*12)/100;
     if (blurRatio >15) {blurAmount = 15;}
     if (lightAmount <15) {lightAmount=15;}
 
+
     $('.no-blur').css('backdrop-filter', 'blur('+blurAmount+'px)'+'brightness('+lightAmount+'%)');
-    
+    $('div#web-professional-box').css('filter', 'opacity('+textOpacity+')');
     //$('.no-blur').css('backdrop-filter', 'brightness('+lightAmount+'%)');
 
 });
