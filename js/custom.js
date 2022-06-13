93f927ee20f570c942d100f4d2a4de1dc901b838
Savvy Insights <greg@savvyinsights.io>
2022-06-12 22:12:32 -0400

    updates

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,7 @@ $(window).scroll(function(e) {
     var distanceScrolled = $(this).scrollTop();
     var blurRatio=distanceScrolled/60;
     var blurAmount = blurRatio;
-    var lightAmount=100-blurRatio*5;
+    var lightAmount=100-blurRatio*8;
     var textOpacity=(100-blurRatio*12)/100;
     if (blurRatio >15) {blurAmount = 15;}
     if (lightAmount <15) {lightAmount=15;}
